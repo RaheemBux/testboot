9ef33829421da4aef46811a1b99fe03451ef0ff6
RaheemBux <raheembux991@gmail.com>
2021-07-03 22:07:27 +0500

    first commit

---
+++ src/main/java/com/example/testapp/controller/Test.java
@@ -0,0 +1,15 @@
+package com.example.testapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Test {
+
+    @GetMapping("/getHelloWorld")
+    public String getHelloWorld(){
+        return "Hello World!!!";
+    }
+}
